fa0aab0433e2254073b54ea63a21d409348781cf
kaw393939 <keith@webizly.com>
2021-10-26 15:48:41 -0400

    removed unused test files

--- tests/add_test.py
+++
@@ -1,9 +0,0 @@
-""" content of calculator.py#"""
-from calculator.main import inc
-
-def test_answer():
-    """This Tests the function"""
-    assert inc(4) == 5
-def test_spam():
-    """This Tests the function"""
-    assert inc(4) == 5
