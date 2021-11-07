736e6ec7b549107245ba87f5be450336988af6ed
njitvjk <90334123+njitvjk@users.noreply.github.com>
2021-11-07 16:09:37 -0500

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -1,30 +1,46 @@
-""" This is the increment function"""
-class Calculator:
-    """ This is the Calculator class"""
+"""Tests file """
+import unittest
+from calculator.main import Calculator
 
-    output = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.output
+class TestApp(unittest.TestCase):
+    """Test for Calculator program"""
+    def setUp(self):
+        """Initialization"""
+        #Arrange
+        self.num_1 = 10
+        self.num_2 = 5
+    def test_0001_add(self):
+        """Case1:Addition"""
+        #Act
+        calc=Calculator()
+        result = calc.addition(self.num_1, self.num_2)
+        #Assert
+        self.assertEqual(result, 15)
+    def test_0002_subtract(self):
+        """Case2 :Subtraction"""
+        calc = Calculator()
+        result = calc.subtraction(self.num_1, self.num_2)
+        self.assertEqual(result, 5)
+    def test_0003_subtract_negativevalue(self):
+        """Case3 :Subtraction_negativeoutput"""
+        calc = Calculator()
+        result = calc.subtraction(self.num_2, self.num_1)
+        self.assertEqual(result, -5)
+    def test_0004_multiply(self):
+        """Case4 :Multiplication"""
+        calc = Calculator()
+        result = calc.multiplication(self.num_1, self.num_2)
+        self.assertEqual(result, 50)
+    def test_0005_division(self):
+        """Case5 :Division"""
+        calc = Calculator()
+        result = calc.division(self.num_1, self.num_2)
+        self.assertEqual(result, 2)
+    def test_0006_divisionbyzero(self):
+        """Case6 :Divisionbyzero"""
+        calc=Calculator()
+        result = calc.division(1,0)
+        self.assertRaises(Exception,result)
 
-    def addition(self, num_x,num_y):
-        """ adds x and y and stores the value in output"""
-        #self.result = self.result + value_a
-        self.output = num_x + num_y
-        return self.output
-    def subtraction(self,num_x,num_y):
-        """ subtract y from x and stores the value in output """
-        self.output = num_x - num_y
-        return self.output
-    def multiplication(self, num_x, num_y):
-        """ multiply x and y and store the value in output"""
-        self.output = num_x * num_y
-        return self.output
-    def division(self, num_x, num_y):
-        """ divide two numbers and store the value in output,Exception handler for divide by zero"""
-        try:
-            self.output = num_x / num_y
-            return self.output
-        except ZeroDivisionError as division_exception:
-            print("Exception:Invalid Input")
-            return division_exception
+if __name__ == '__main__':
+    unittest.main()
