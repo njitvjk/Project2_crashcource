8017cb400e6638a6ff80d5922c74bf04ade83de3
njitvjk <kv332@njit.edu>
2021-11-07 08:58:16 -0500

    Updated Calculator test and main file

--- calculator/main.py
+++ calculator/main.py
@@ -28,36 +28,3 @@ class Calculator:
         except ZeroDivisionError as division_exception:
             print("Exception:Invalid Input")
             return division_exception
-
-#Calculator from Console
-calc = Calculator()
-
-while True:
-
-    print("1: Add")
-    print("2: Subtract")
-    print("3: Multiply")
-    print("4: Divide")
-    print("5: Exit")
-
-    choice = int(input("Select operation: "))
-    " Make sure the user have entered the valid choice"
-    if choice in (1, 2, 3, 4, 5):
-        # first check whether user want to exit
-        if choice == 5:
-            break
-        # if not exit then show user with choices and perform operation
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
-        if choice == 1:
-            print(num1, "+", num2, "=",Calculator.addition(Calculator,num1,num2))
-        elif choice == 2:
-            print(num1, "-", num2, "=", Calculator.subtraction(Calculator,num1,num2))
-        elif choice == 3:
-            print(num1, "*", num2, "=", Calculator.multiplication(Calculator,num1,num2))
-        elif choice == 4:
-            if num2==0:
-                print(Calculator.division(Calculator,num1,num2))
-            print(num1, "/", num2, "=",Calculator.division(Calculator,num1,num2))
-    else:
-        print("Invalid Input")
