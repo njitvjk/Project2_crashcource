e954528635afaca69dcca55037469b86a5bd69b0
njitvjk <kv332@njit.edu>
2021-11-07 08:53:15 -0500

    Updated Calculator test and main file

--- calculator/main.py
+++ calculator/main.py
@@ -2,20 +2,62 @@
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
+    output = 0
     def get_result(self):
         """ Get Result of Calculation"""
-        return self.result
+        return self.output
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+    def addition(self, num_x,num_y):
+        """ adds x and y and stores the value in output"""
+        #self.result = self.result + value_a
+        self.output = num_x + num_y
+        return self.output
+    def subtraction(self,num_x,num_y):
+        """ subtract y from x and stores the value in output """
+        self.output = num_x - num_y
+        return self.output
+    def multiplication(self, num_x, num_y):
+        """ multiply x and y and store the value in output"""
+        self.output = num_x * num_y
+        return self.output
+    def division(self, num_x, num_y):
+        """ divide two numbers and store the value in output,Exception handler for divide by zero"""
+        try:
+            self.output = num_x / num_y
+            return self.output
+        except ZeroDivisionError as division_exception:
+            print("Exception:Invalid Input")
+            return division_exception
+
+#Calculator from Console
+calc = Calculator()
+
+while True:
+
+    print("1: Add")
+    print("2: Subtract")
+    print("3: Multiply")
+    print("4: Divide")
+    print("5: Exit")
+
+    choice = int(input("Select operation: "))
+    " Make sure the user have entered the valid choice"
+    if choice in (1, 2, 3, 4, 5):
+        # first check whether user want to exit
+        if choice == 5:
+            break
+        # if not exit then show user with choices and perform operation
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        if choice == 1:
+            print(num1, "+", num2, "=",Calculator.addition(Calculator,num1,num2))
+        elif choice == 2:
+            print(num1, "-", num2, "=", Calculator.subtraction(Calculator,num1,num2))
+        elif choice == 3:
+            print(num1, "*", num2, "=", Calculator.multiplication(Calculator,num1,num2))
+        elif choice == 4:
+            if num2==0:
+                print(Calculator.division(Calculator,num1,num2))
+            print(num1, "/", num2, "=",Calculator.division(Calculator,num1,num2))
+    else:
+        print("Invalid Input")
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,32 +1,54 @@
 """Testing the Calculator"""
 from calculator.main import Calculator
+#Arrange
+NUM1=5
+NUM2=10
 
-def test_calculator_result():
+def calculator_result():
     """testing calculator result is 0"""
+    #Act
     calc = Calculator()
-    assert calc.result == 0
+    #Assert
+    assert calc.output == 0
 
-def test_calculator_add():
+def calculator_add():
     """Testing the Add function of the calculator"""
     #Arrange by instantiating the calc class
     calc = Calculator()
     #Act by calling the method to be tested
-    calc.add_number(4)
+    calc.addition(NUM1,NUM2)
     #Assert that the results are correct
-    assert calc.result == 4
+    assert calc.output == 15
 
-def test_calculator_get_result():
+def calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
 
-def test_calculator_subtract():
+def calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-def test_calculator_multiply():
+    calc.subtraction(NUM2,NUM1)
+    assert calc.get_result() == 5
+
+def calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
+    result  = calc.multiplication(NUM1,NUM2)
+    assert result == 50
+
+def calculator_divide():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result = calc.division(NUM2, NUM1)
     assert result == 2
+
+def calculator_subtractnegative():
+    """Testing the subtract method of the calculator"""
+    calc = Calculator()
+    calc.subtraction(NUM1,NUM2)
+    assert calc.get_result() == -5
+
+def calculator_divisionbyzero(self):
+    """Testing division by zero"""
+    self.assertRaises(ZeroDivisionError, calculator_divide(), 1, 0)
