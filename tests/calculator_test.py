6d562684b7ebb1a2113c909d8361774d4438e077
njitvjk <kv332@njit.edu>
2021-11-16 12:23:03 -0500

    calculator- part 3 assignment update

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,89 +1,47 @@
-"""Testing the Arithematic operations"""
+"""Testing the Calculator"""
 import pytest
-
 from calculator.calculator import Calculator
+from history.calculations import Calculations
 @pytest.fixture
-def clear_history():
-    """fixture function to run before every functions defined below"""
-    Calculator.clear_history()
-def test_001_add_to_history():
-    """Case1 :Add calculation to history"""
-    Calculator.add_numbers(1, 2)
-    Calculator.divide_numbers(5, 1)
-    assert Calculator.history_count() == 2
-    assert Calculator.get_history() == 5
+def clear_history_fixture():
+    """define a function that will run each time you pass it to a test, it is called a fixture"""
+    # pylint: disable=redefined-outer-name
+    Calculations.clear_history()
+#You have to add the fixture function as a parameter to the test that you want to use it with
+def test_calculator_add_static(clear_history_fixture):
+    """testing that our calculator has a static method for addition"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    #using Tuple instead of args because we can pack as much data as we need into the tuple
+    my_tuple = (1.0,2.0,5.0)
+    Calculator.add_numbers(my_tuple)
+    assert Calculator.get_last_result_value() == 8.0
+def test_calculator_subtract_static(clear_history_fixture):
+    """Testing the subtract method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    #using Tuple instead of args because we can pack as much data as we need into the tuple
+    my_tuple = (1.0,2.0,3.0)
+    Calculator.subtract_number(my_tuple)
+    assert Calculator.get_last_result_value() == -6.0
 
-def test_002_add_values():
-    """Case1:Add two numbers"""
+def test_calculator_multiply_static(clear_history_fixture):
+    """Testing the Multiplication method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.add_numbers(1,2) == 3
-    assert Calculator.add_numbers(2, 2) == 4
-    assert Calculator.add_numbers(3, 2) == 5
-    assert Calculator.add_numbers(4, 2) == 6
-def test_003_clear_history(clear_history):
-    """Case2:clear history"""
+    #using Tuple instead of args because we can pack as much data as we need into the tuple
+    my_tuple = (1.0,2.0,3.0)
+    Calculator.multiply_numbers(my_tuple)
+    assert Calculator.get_last_result_value() == 6.0
+
+def test_calculator_division(clear_history_fixture):
+    """Testing the division method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.add_numbers(1,2) == 3
-    assert Calculator.add_numbers(2, 2) == 4
-    assert Calculator.add_numbers(3, 2) == 5
-    assert Calculator.add_numbers(4, 2) == 6
-    assert Calculator.history_count() == 4
-    assert Calculator.clear_history() is True
-    assert Calculator.history_count() == 0
-def test_004_count_history(clear_history):
-    """Case3:Count history"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.history_count() == 0
-    assert Calculator.add_numbers(2, 2) == 4
-    assert Calculator.add_numbers(3, 2) == 5
-    assert Calculator.history_count() == 2
-def test_005_get_last_result(clear_history):
-    """Case4: get last result"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.add_numbers(2, 2) == 4
-    assert Calculator.add_numbers(3, 2) == 5
-    assert Calculator.get_result_of_last_calculation() == 5
-def test_006_get_last(clear_history):
-    """Case5 :test last result"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    Calculator.add_numbers(2, 2)
-    Calculator.divide_numbers(5, 5)
-    assert Calculator.history_count() == 2
-    assert Calculator.get_last_calculation() == 1
-def test_007_get_last_object(clear_history):
-    """Case6 :test last result"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    Calculator.divide_numbers(4, 2)
-    Calculator.divide_numbers(4, 1)
-    assert Calculator.history_count() == 2
-    assert Calculator.get_last_calculation_object() == 4
-def test_008_get_first_result(clear_history):
-    """Case5: get first result"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.add_numbers(2, 2) == 4
-    assert Calculator.add_numbers(3, 2) == 5
-    assert Calculator.get_result_of_first_calculation() == 4
-def test_009_subtraction_values(clear_history):
-    """Case6:test subtraction"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.subtract_number(1, 2) == -1
-def test_010_multiplication_values(clear_history):
-    """ Case7 :multiplication of two numbers"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.multiply_numbers(1,2) == 2
-def test_011_division_values(clear_history):
-    """Case9 :Division"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.divide_numbers(2, 2) == 1
-def test_012_get_history(clear_history):
-    """Case10 :get history returns the last calculation result"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    Calculator.add_numbers(2, 2)
-    Calculator.divide_numbers(2, 2)
-    assert Calculator.get_history() ==1
-def test_013_division_by_zero(clear_history):
+    #using Tuple instead of args because we can pack as much data as we need into the tuple
+    my_tuple = (100.0,10.0,2.0)
+    Calculator.divide_numbers(my_tuple)
+    assert Calculator.get_last_result_value() == 0.05
+
+@pytest.mark.xfail(raises=ZeroDivisionError)
+def test_calculation_division_by_zero(clear_history_fixture):
     """Case11 :Division by zero"""
     # pylint: disable=unused-argument,redefined-outer-name
-    with pytest.raises(ZeroDivisionError) as exception_info:
-        Calculator.divide_numbers(1, 0)
-    assert "ZeroDivisionError" in str(exception_info.type)
+    my_tuple = (0.0,1.0)
+    Calculator.divide_numbers(my_tuple)
