969fd6de0fa23868e7a3dd00f06564f190c416a9
njitvjk <kv332@njit.edu>
2021-11-20 05:13:45 -0500

    Committing AAA demonstration

--- calculator/main.py
+++ calculator/main.py
@@ -1,26 +1,30 @@
 """Calculator program """
 
+
 class Calculator:
     """Class with arithematic operations """
-    output=0
-    def addition(self, num_1:int, num_2:int):
+    output = 0
+
+    def addition(self, num_1, num_2):
         """Perform addition operation on num_1 and num_2"""
-        self.output=num_1+num_2
+        self.output = float(num_1) + float(num_2)
         return self.output
-    def subtraction(self, num_1:int, num_2:int):
+
+    def subtraction(self, num_1, num_2):
         """Perform subtraction operation on num_! and num_2 """
-        self.output = num_1 - num_2
+        self.output = float(num_1) - float(num_2)
         return self.output
-    def division(self, num_1:int, num_2:int):
+
+    def division(self, num_1, num_2):
         """Perform division operation on num_1 and num_2"""
-        #if b == 0:
+        # if b == 0:
         try:
-            self.output = num_1/num_2
+            self.output = float(num_1) / float(num_2)
             return self.output
         except ZeroDivisionError as ex_div:
-            return ex_div
+            print('cannot divide by zero')
 
-    def multiplication(self, num_1:int, num_2:int):
+    def multiplication(self, num_1, num_2):
         """Perform multiplication operation on a and b """
-        self.output = num_1 *num_2
+        self.output = float(num_1) * float(num_2)
         return self.output
--- operations/division.py
+++ operations/division.py
@@ -2,14 +2,15 @@
 
 from operations.calculation_base import Calculation
 
+
 class Division(Calculation):
     """ Division of two values a and b inherited from Parent class"""
 
     def get_output(self):
         """Function also checks for zero division error"""
-        #return self.value_a / self.value_b
+        # return self.value_a / self.value_b
         try:
             return self.value_a / self.value_b
-        except ZeroDivisionError  as ex_div:
+        except ZeroDivisionError as ex_div:
             print('Invalid Denominator Input:', ex_div)
             raise ZeroDivisionError from ex_div
