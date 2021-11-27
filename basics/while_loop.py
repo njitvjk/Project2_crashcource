fedd80caeb75cad817fb689ec29cf4a326c01c34
njitvjk <kv332@njit.edu>
2021-11-26 19:03:36 -0500

    commited for and while loop

---
+++ basics/while_loop.py
@@ -0,0 +1,16 @@
+# Program to add numbers up to n
+# sum = 1+2+3+...+n
+
+n = 10
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i+1    # update counter
+
+# print the sum
+print("The sum is", sum)
+
