d2e295bbd4b949a0dfeae17c6a3d884da2edcc87
njitvjk <kv332@njit.edu>
2021-12-01 20:13:26 -0500

    final commit

--- calculator/main.py
+++ calculator/main.py
@@ -1,20 +1,25 @@
+"""Watchdog module"""
 import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+
 class Handler(FileSystemEventHandler):
     """Class implements watch dog to watch a folder and move the input file to another folder"""
-    def on_modified(self,event):
-        for filename in os.listdir(folder_to_track):
-            src = folder_to_track + '/' + filename
-            new_dest = folder_destination + '/' + filename
+
+    def on_modified(self, event):
+        for filename in os.listdir(FOLDER_TO_TRACK):
+            src = FOLDER_TO_TRACK + '/' + filename
+            new_dest = FOLDER_DESTINATION + '/' + filename
             os.rename(src, new_dest)
 
-folder_to_track = 'input'
-folder_destination = 'done'
+
+FOLDER_TO_TRACK = 'input'
+FOLDER_DESTINATION = 'done'
 observer = Observer()
 event_handler = Handler()
-observer.schedule(event_handler, folder_to_track)
+observer.schedule(event_handler, FOLDER_TO_TRACK)
 print("watchDog starts")
 observer.start()
 print("watchdog After the start")
